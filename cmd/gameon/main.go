afa86726b9986a86bdaee9950ec05a071ebb9662
Etai Lev Ran <etail@il.ibm.com>
2016-10-10 01:05:03 +0300

    added request signing, implemented initial delete room flow
    added null and GameOn request signers
    added an http.Client based Doer
    added tracing functionality to sub command processing
    sub-commands are provided with server URL for constructing requests. GameOn identifier also provided, although may not be required...

--- cmd/gameon/main.go
+++ cmd/gameon/main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/url"
 	"os"
 	"path"
@@ -23,22 +24,24 @@ func main() {
 	//
 	// see https://github.com/gameontext/gameon-room-go#get-game-on-id-and-shared-secret for directions
 	// on generating and retrieving the shared secret and id values
-	var verbose bool        // CLI verbosity, boolean flags (default: false)
+	var debug bool          // CLI debug (i.e., verbosity), boolean flags (default: false)
 	var sharedSecret string // GameOn! shared secret, required. Set in command line or GAMEON_SECRET env-var
 	var identity string     // GameOn! identity, required. Set in command line or GAMEON_ID env-var
 	var server urlFlag      // GameOn! server URL
 
-	addCommonFlags(&verbose, &sharedSecret, &identity, &server)
+	addCommonFlags(&debug, &sharedSecret, &identity, &server)
+	flag.Parse()
+	err := validateRequiredCommonFlags(debug, sharedSecret, identity, server.Url())
+	if err != nil {
+		usage(err.Error(), nil)
+		os.Exit(1)
+	}
 
 	//-- Sub-commands
-	subcommands := []subcommand{deleteRoomSubcommand(), listRoomsSubcommand(), registerRoomSubcommand()}
-
-	flag.Parse()
-	err := validateRequiredCommonFlags(verbose, sharedSecret, identity, server.Url())
-
-	if err != nil {
-		usage(err.Error(), subcommands)
-		os.Exit(1)
+	subcommands := []subcommand{
+		deleteRoomSubcommand(*server.Url(), identity),
+		listRoomsSubcommand(*server.Url(), identity),
+		registerRoomSubcommand(*server.Url(), identity),
 	}
 
 	tail := flag.Args() // left-over, unprocessed, positional args
@@ -57,13 +60,17 @@ func main() {
 				os.Exit(1)
 			}
 
-			// create the GameOn client
-			err = sc.Process(nil)
+			tracer := ioutil.Discard
+			if debug {
+				tracer = os.Stdout
+			}
+			_, err = sc.Process(nil, newSigner(identity, sharedSecret), tracer)
 			if err != nil {
 				fmt.Println(err)
 				os.Exit(1)
 			}
-			return // all done, command completed successfully
+
+			return // all done, command completed successfully (TODO: capture and report status code?)
 		}
 	}
 
@@ -77,27 +84,27 @@ func main() {
 	os.Exit(1)
 }
 
-func addCommonFlags(verbose *bool, secret *string, identity *string, serverUrl *urlFlag) {
+func addCommonFlags(debug *bool, secret *string, identity *string, serverUrl *urlFlag) {
+	flag.BoolVar(debug, "d", false, "Enable debug (optional, default: false)")
+	flag.Var(serverUrl, "g", "GameOn! server URL (required)")
 	flag.StringVar(identity, "id", os.Getenv("GAMEON_ID"),
 		"GameOn! identity (required, default: $GAMEON_ID environment variable)")
 	flag.StringVar(secret, "secret", os.Getenv("GAMEON_SECRET"),
 		"GameOn! shared secret (required, default: $GAMEON_SECRET environment variable)")
-	flag.BoolVar(verbose, "d", false, "Enable debug (optional, default: false)")
-	flag.Var(serverUrl, "g", "GameOn! server URL (required)")
 }
 
 // validate that (required) flags have been successfully parsed
-func validateRequiredCommonFlags(verbose bool, secret string, identity string, gos *url.URL) error {
-	if identity == "" {
+func validateRequiredCommonFlags(debug bool, secret string, identity string, gameOnURL *url.URL) error {
+	if gameOnURL == nil {
+		return fmt.Errorf("no valid URL for GameOn! server")
+	} else if identity == "" {
 		return fmt.Errorf("identity not provided in command line or $GAMEON_ID environment variable")
 	} else if secret == "" {
 		return fmt.Errorf("shared secret not provided in command line or $GAMEON_SECRET environment variable")
-	} else if gos == nil {
-		return fmt.Errorf("no valid URL for GameOn! server")
 	}
 
-	if verbose {
-		fmt.Printf("id:%s secret:%s server:%s verbose:%t\r\n", identity, secret, gos.String(), verbose)
+	if debug {
+		fmt.Printf("id:%s secret:%s server:%s verbose:%t\r\n", identity, secret, gameOnURL.String(), debug)
 	}
 
 	return nil
@@ -108,8 +115,10 @@ func usage(message string, subcommands []subcommand) {
 	fmt.Println("Usage:\r\n\t", path.Base(os.Args[0]), "<global flags>", "<sub-command> [sub-command flags]")
 	fmt.Println("\r\nGlobal flags:")
 	flag.PrintDefaults()
-	fmt.Println("\r\nSub-commands:")
-	for _, sc := range subcommands {
-		fmt.Println("  ", sc.Usage())
+	if subcommands != nil {
+		fmt.Println("\r\nSub-commands:")
+		for _, sc := range subcommands {
+			fmt.Println("  ", sc.Usage())
+		}
 	}
 }
--- cmd/gameon/subcommand.go
+++ cmd/gameon/subcommand.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"net/url"
+	"runtime"
 )
 
 // The subcommand interface defines methods to allow the main function to work with sub-command generically.
 type subcommand interface {
-	Keyword() string           // sub-command keyword
-	Usage() string             // usage text
-	Parse(argv []string) error // parse command line arguments
-	Process(client Doer) error // do actual work, using the given Doer (if nil, creates a default one)
+	Keyword() string                                // sub-command keyword
+	Usage() string                                  // usage text
+	Parse([]string) error                          // parse command line arguments
+	Process(Doer, Signer, io.Writer) (bool, error) // do work
+	// Uses the given Doer, Signer and io.Writer (for tracing). if Doer or Signer are nil, will internally
+	// create a default implementation.
+	// TODO: Returns?
 }
 
 //-----------------------------------------------------------------------------
@@ -22,17 +28,23 @@ type subcommand interface {
 
 //-- delete a room by its identifier
 type deleteRoom struct {
-	roomID string // identifier of room to delete
+	server   url.URL // GameOn! server
+	gameonID string  // GameOn! id
+	roomID   string  // identifier of room to delete
 }
 
 // create a new delete sub-command instance, along with its flags set
-func deleteRoomSubcommand() *deleteRoom {
-	return &deleteRoom{}
+func deleteRoomSubcommand(server url.URL, goid string) *deleteRoom {
+	return &deleteRoom{
+		server:   server,
+		gameonID: goid,
+	}
 }
 
 // implement GoStringer, mostly for debugging/tracing in verbose mode
 func (del *deleteRoom) GoString() string {
-	return fmt.Sprintf("%s - roomid:%s", del.Keyword(), del.roomID)
+	return fmt.Sprintf("%s - roomid:%s gameonID:%s server:%s", del.Keyword(), del.roomID, del.gameonID,
+		del.server.String())
 }
 
 // sub-command's keyword
@@ -55,24 +67,95 @@ func (del *deleteRoom) Parse(argv []string) error {
 	return nil
 }
 
-// execute the delete room functionality using the given Doer. If Doer is nil, Process will create a default
-// implementation. The use of Doer is to allow customization of the (http/https) client used (e.g., for testing)
-func (del *deleteRoom) Process(client Doer) error {
-	return nil
+// Attempts to delete the room denoted by roomID from a Game On! server at server.
+// The delete room functionality uses the given Doer. If Doer is nil, it'll create a default implementation.
+// Flow is reported via the tracer, which can be set by the caller.
+func (del *deleteRoom) Process(client Doer, auth Signer, trace io.Writer) (bool, error) {
+	fmt.Fprintln(trace, location(), del, "Doer", client, "Signer", auth)
+
+	// validate requirements
+	if del.server.String() == "" || del.gameonID == "" || del.roomID == "" {
+		return false, fmt.Errorf("%#v", del)
+	}
+
+	httpClient, authenticator := client, auth
+	if httpClient == nil { // using default as a fallback, caller explicitly said it doesn't care
+		httpClient = newDefaultDoer(del.server)
+	}
+	if authenticator == nil { // caller doesn't care, use minimal to get by...
+		authenticator = nullSigner{}
+	}
+
+	u := del.server
+	u.Path = "/map/v1/sites/" + del.roomID
+
+	fmt.Fprintln(trace, "DELETE", u.String())
+	req, err := http.NewRequest("DELETE", u.String(), nil)
+	if err != nil {
+		fmt.Fprintln(trace, location(), "NewRequest.Error", err.Error())
+		return false, err
+	}
+
+	_ = authenticator.Sign(req) // TODO: handle error
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		fmt.Fprintln(trace, location(), "DELETE.Error", err.Error())
+		return false, err
+	}
+
+	fmt.Fprintln(trace, "Status", resp.StatusCode)
+
+	/*defer r.Body.Close()
+	b, e := ioutil.ReadAll(r.Body)
+	if e == nil {
+		body = string(b)
+	}*/
+	/* io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close() */
+
+	/*body, err := extractBody(resp)
+	if err != nil {
+		checkpoint(locus, fmt.Sprintf("Body.Error err=%s", err.Error()))
+		return
+	}
+	checkpoint(locus, fmt.Sprintf("Status=%s", resp.Status))
+
+	switch resp.StatusCode {
+	case http.StatusNoContent:
+		checkpoint(locus, "Deleted")
+		return
+	case http.StatusOK, http.StatusForbidden, http.StatusNotFound:
+		checkpoint(locus, "Sigh. There is no use trying any more.")
+		printResponseBody(locus, resp, body)
+		stopTrying = true
+		return
+	default:
+		err = RegError{fmt.Sprintf("Unhandled Status: %s", resp.Status)}
+		checkpoint(locus, fmt.Sprintf("Unhandled Status=%s", resp.Status))
+		printResponseBody(locus, resp, body)
+		return
+	}*/
+
+	return true, nil
 }
 
 //-- list registered rooms by owner and (optionally) name
 type listRooms struct {
-	name  string // room owner (e.g., email)
-	owner string // room name, if multiple rooms have been registered
+	server   url.URL // GameOn! server
+	gameonID string  // GameOn! id (owner field in queries, e.g., email)
+	roomName string  // room name, if multiple rooms have been registered
 }
 
-func listRoomsSubcommand() *listRooms {
-	return &listRooms{}
+func listRoomsSubcommand(server url.URL, goid string) *listRooms {
+	return &listRooms{
+		server:   server,
+		gameonID: goid,
+	}
 }
 
 func (ls *listRooms) GoString() string {
-	return fmt.Sprintf("%s - owner:%s name:%s", ls.Keyword(), ls.owner, ls.name)
+	return fmt.Sprintf("%s - owner:%s name:%s server:%s", ls.Keyword(), ls.gameonID, ls.roomName,
+		ls.server.String())
 }
 
 func (ls *listRooms) Keyword() string {
@@ -80,38 +163,41 @@ func (ls *listRooms) Keyword() string {
 }
 
 func (ls *listRooms) Usage() string {
-	return fmt.Sprintf("%s %s %s", ls.Keyword(), "<room-owner>", "[room-name-to-match]")
+	return fmt.Sprintf("%s %s", ls.Keyword(), "[room-name-to-match]")
 }
 
 func (ls *listRooms) Parse(argv []string) error {
 	argc := len(argv)
-	if argc < 1 || argc > 2 {
+	if argc > 2 {
 		return fmt.Errorf("%s", ls.Usage())
-	}
-
-	ls.owner = argv[0]
-	if argc == 2 {
-		ls.name = argv[1]
+	} else if argc == 1 {
+		ls.roomName = argv[0]
 	}
 	return nil
 }
 
-func (ls *listRooms) Process(client Doer) error {
-	return nil
+func (ls *listRooms) Process(client Doer, _ Signer, trace io.Writer) (bool, error) {
+	return true, nil
 }
 
 //-- register a new room
 type registerRoom struct {
-	name     string
-	callback *url.URL
+	server      url.URL  // GameOn! server
+	gameonID    string   // GameOn! id
+	roomName    string   // room name
+	callbackURL *url.URL // room callback URL (must be accessible from GameOn! server)
 }
 
-func registerRoomSubcommand() *registerRoom {
-	return &registerRoom{}
+func registerRoomSubcommand(server url.URL, goid string) *registerRoom {
+	return &registerRoom{
+		server:   server,
+		gameonID: goid,
+	}
 }
 
 func (reg *registerRoom) GoString() string {
-	return fmt.Sprintf("%s - name: %s cb:%s %#v", reg.Keyword(), reg.name, reg.callback)
+	return fmt.Sprintf("%s - name:%s cb:%s server:%s", reg.Keyword(), reg.roomName, reg.callbackURL.String(),
+		reg.server.String())
 }
 
 func (reg *registerRoom) Keyword() string {
@@ -128,16 +214,23 @@ func (reg *registerRoom) Parse(argv []string) error {
 	}
 
 	var err error
-	reg.callback, err = url.Parse(argv[1])
+	reg.callbackURL, err = url.Parse(argv[1])
 
 	if err != nil {
 		return err
 	}
 
-	reg.name = argv[0]
+	reg.roomName = argv[0]
 	return nil
 }
 
-func (reg *registerRoom) Process(client Doer) error {
-	return nil
+func (reg *registerRoom) Process(client Doer, auth Signer, trace io.Writer) (bool, error) {
+	return true, nil
+}
+
+// current program location (file, line and function name)
+func location() string {
+	pc, file, line, _ := runtime.Caller(1)
+	funcname := runtime.FuncForPC(pc).Name()
+	return fmt.Sprintf("%s:%d %s", file, line, funcname)
 }
