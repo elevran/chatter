9745b7706ee67723514818906b36cc81fac96d38
Zvi Cahana <zvic@il.ibm.com>
2016-10-19 13:15:15 +0300

    add room service config

--- cmd/room/main.go
+++ cmd/room/main.go
@@ -2,19 +2,32 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"os"
+
+	"fmt"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/urfave/cli"
 )
 
 func main() {
-	room := newChatRoom()
+	app := cli.NewApp()
+	app.Name = "mediator"
+	app.Flags = Flags
+	app.Action = func(context *cli.Context) error {
+		config := newConfig(context)
+		room := newChatRoom()
 
-	http.HandleFunc("/hello", room.hello)
-	http.HandleFunc("/goodbye", room.goodbye)
-	http.HandleFunc("/room", room.message)
-	err := http.ListenAndServe(":80", nil)
+		http.HandleFunc("/hello", room.hello)
+		http.HandleFunc("/goodbye", room.goodbye)
+		http.HandleFunc("/room", room.message)
+		return http.ListenAndServe(fmt.Sprintf(":%d", config.HTTPPort), nil)
 
+	}
+
+	err := app.Run(os.Args)
 	if err != nil {
-		fmt.Println(err)
+		logrus.WithError(err).Fatalf("Error running main")
 	}
 }
